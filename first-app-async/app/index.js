ac4673a848e2bba5607db12d1b0759c16f2ffa99
Orange Team <alessandrfenu@gmail.com>
2024-09-10 15:42:16 +0000

    Day 1: First app + First app async

--- first-app-async/app/index.js
+++ first-app-async/app/index.js
@@ -3,7 +3,20 @@ const mathUtils = require("./math-utils");
 let i = 1;
 
 otherIncomingRequests();
-longRequest(50000);
+
+let from = 2;
+let to = 100;
+let max = 10000;
+const interval = setInterval(() => {
+    console.log("primes: " + longRequest(from, to));
+    if(to >= max){
+        clearInterval(interval);
+    }
+    from = to;
+    to = to + 100;
+}, 50);
+
+
 
 /**
  * DO NOT CHANGE IT
@@ -19,12 +32,12 @@ function otherIncomingRequests() {
  * @param {*} n
  * @returns
  */
-function longRequest(n) {
+function longRequest(from, n) {
     let id = i++;
-    console.log(`Id: ${id}. Starting blocking request. Find primes from 2 to ${n}`);
+    console.log(`Id: ${id}. Starting blocking request. Find primes from ${from} to ${n}`);
     const start = new Date();
 
-    const primes = mathUtils.getPrimeNumbersWithinRange(2, n);
+    const primes = mathUtils.getPrimeNumbersWithinRange(from, n);
 
     const end = new Date();
     console.log(`Id: ${id}. Finished blocking request. Elapsed ms: ${end.getTime() - start.getTime()}`);
